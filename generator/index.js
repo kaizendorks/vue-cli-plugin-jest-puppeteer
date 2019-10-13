8180db65d50ea3462d1a81d722459c2a1a6d54eb
Daniel Jimenez Garcia <daniel.garcia@oliverwyman.com>
2019-10-13 12:58:35 +0200

    publish new version

--- generator/index.js
+++ generator/index.js
@@ -3,16 +3,17 @@ module.exports = (api, options) => {
 
   // These dependencies always need to be added
   const devDependencies = api.hasPlugin('unit-jest')
-    // If the official @vue/cli-plugin-unit-jest installed, then we can avoid installing jest and some related dependencies
+    // If the official @vue/cli-plugin-unit-jest installed,
+    // then we can avoid installing jest and some related dependencies
     // However since it still uses jest 23, the version of jest-puppeteer cannot be the last one
     ? {
       puppeteer: '^1.17.0',
-      'jest-puppeteer': '^3.9.0'
+      'jest-puppeteer': '^3.9.0',
     }
     // The plugin isnt installed, so we can install latest jest as well as latest jest-puppeteer
     : {
       jest: '^24.8.0',
-      'babel-jest': "^24.8.0",
+      'babel-jest': '^24.8.0',
       'babel-plugin-transform-es2015-modules-commonjs': '^6.26.2',
       puppeteer: '^1.17.0',
       'jest-puppeteer': '^4.2.0',
@@ -25,5 +26,5 @@ module.exports = (api, options) => {
     },
   });
 
-  api.exitLog('Run the tests with "npm run test:e2e"', 'info')
+  api.exitLog('Run the tests with "npm run test:e2e"', 'info');
 };
